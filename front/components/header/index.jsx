b925f57bee72e24e6c00ded94070439366ccaf0b
lizill <lizill@naver.com>
2021-09-18 23:04:49 +0900

    header

--- front/components/header/index.jsx
+++ front/components/header/index.jsx
@@ -1,30 +1,14 @@
-import React, {useState, useEffect, useRef} from "react";
+import React, { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import Link from "next/link";
-// import classNames from 'classnames';
 
 import styles from './styles/header.module.scss'
 
 const Header = () => {
   const { t } = useTranslation("common");
-  const [changeHeader, setChangeHeader] = useState(false);
-  const target = useRef(null);
-  const onIntersect = ([entry], observer) => {
-    if (entry.isIntersecting) {
-      observer.unobserve(entry.target);
-      setChangeHeader(true);
-      observer.observe(entry.target);
-    }
-  }
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(onIntersect, { threshold: 1.0 });
-    observer.observe(target.current);
-    return () => observer.disconnect();
-  }, []);
 
   return (
-    <header ref={target} className={changeHeader < 100 ? styles.change_header : styles.header}>
+    <header className={styles.header}>
       <menu className={styles.menu}>
         <p>{t("title")}</p>
         <div className={styles.aTags}>
