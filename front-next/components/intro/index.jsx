ce9a91c606e09c704f79a78a5125e12b8f3d8c16
lizill <lizill@naver.com>
2021-09-24 20:48:25 +0900

    set stars, shooting star

--- front-next/components/intro/index.jsx
+++ front-next/components/intro/index.jsx
@@ -1,24 +1,73 @@
-import React from 'react'
-import Wave from 'react-wavify'
+import React, { useEffect, useState } from 'react';
+import Wave from 'react-wavify';
 
 import styles from './styles/intro.module.css';
 
 const Intro = () => {
+  const [scrollY, setScrollY] = useState(0);
+  const [starArr, setStarArr] = useState([]);
+  const [shootingLocate, setShootingLocate] = useState(null);
+
+  const settingStarArr = () => {
+    // 1920 * 1080 / 10000 = 207.3600
+    let starsCount = window.innerWidth * window.innerHeight / 10000
+    let stars = [];
+    for(let i = 0; i<starsCount; i++) {
+      let toLeft = Math.round(Math.random() * window.innerWidth);
+      let toTop = Math.round(Math.random() * window.innerHeight);
+      stars.push({ id: i, toLeft, toTop });
+    }
+    setStarArr((prevArr) => prevArr = stars);
+    settingShootingStar()
+  }
+
+  const settingShootingStar = () => {
+    let toLeft = Math.round(Math.random() * window.innerWidth);
+    let toTop = Math.round(Math.random() * window.innerHeight);
+    setShootingLocate({ top: toTop, left: toLeft });
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => setScrollY(window.scrollY));
+    return () => {
+      window.removeEventListener('scroll', () => setScrollY(window.scrollY));
+    }
+  }, [scrollY]);
+
+  useEffect(() => {
+    window.addEventListener('resize', settingStarArr);
+    settingStarArr();
+    return () => {
+      window.removeEventListener('resize', settingStarArr);
+    }
+  }, []);
+
   return (
     <div className={styles.introWrap}>
+      {starArr && starArr.map(v => (
+        <div className={styles.star} style={{ top: v.toTop, left: v.toLeft }} key={v.id}/>
+      ))}
+      {shootingLocate && (
+        <div className={styles.shootingWrap} style={{ top: shootingLocate.top, left: shootingLocate.left }}>
+          <div className={styles.shooting_star}/>
+        </div>
+      )}
       <div className={styles.moonWrap}>
         <img src="/images/intro/moon.png" alt="moon" />
       </div>
-      <Wave className={styles.wave} fill='#040037'
-        paused={false}
-        options={{
-          height: 20,
-          amplitude: 30,
-          speed: 0.5,
-          points: 3
-        }}
-      />
-      <div className={styles.intro}>
+      <div className={styles.waveWrap}>
+        <Wave className={styles.wave} fill='#040037'
+          paused={false}
+          options={{
+            height: 20,
+            amplitude: 30,
+            speed: 0.4,
+            points: 3
+          }}
+          style={{ height: 150 + scrollY/3 }}
+        />
+      </div>
+      <div className={styles.title}>
         <h1>ドンの</h1>
         <h1>ポートフォリオ</h1>
         <p>1ST PORTFOLIO</p>
@@ -31,7 +80,7 @@ const Intro = () => {
         options={{
           height: 20,
           amplitude: 20,
-          speed: 0.4,
+          speed: 0.8,
           points: 3
         }}
       />
